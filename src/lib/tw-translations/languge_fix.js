7ef16e365e9b8585f62546836261ddd1a9032061
KOSHINOawa <2381068747@qq.com>
2025-11-27 21:11:58 +0800

    fix

--- src/lib/tw-translations/languge_fix.js
+++ src/lib/tw-translations/languge_fix.js
@@ -1,5 +1,5 @@
 // 引入 TurboWarp 的翻译系统 API
-import { addTranslations } from '@turbowarp/l10n';
+import { addTranslations } from '@turbowarp/scratch-l10n';
 
 // bro tw的翻译有些是错误的，这里要修正一下下
 addTranslations('zh-cn', {
