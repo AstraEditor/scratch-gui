0f9aed92d1078dd6c6e0549b59e861d706bed3bc
KOSHINO <2381068747@qq.com>
2026-03-05 20:54:48 +0800

    Update _manifest_entry.js

--- src/addons/addons/sprite-folders/_manifest_entry.js
+++ src/addons/addons/sprite-folders/_manifest_entry.js
@@ -1,7 +1,7 @@
 const manifest = {
     name: 'Sprite File List View',
     description: 'Transforms the sprite list into a VSCode-style file explorer with folders (works with // folder naming).',
-    tags: ['editor', 'sprites', 'MistWarp'],
+    tags: ['theme', 'new', 'MistWarp'],
     userscripts: [
         {
             url: 'userscript.js'
