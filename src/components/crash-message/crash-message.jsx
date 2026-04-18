38b22420d5b6e733b039d181a425a8d32b105019
KOSHINO <2381068747@qq.com>
2026-04-18 17:45:17 +0800

    Work currently

--- src/components/crash-message/crash-message.jsx
+++ src/components/crash-message/crash-message.jsx
@@ -1,14 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {connect} from 'react-redux';
 import Box from '../box/box.jsx';
 import { FormattedMessage } from 'react-intl';
+import downloadBlob from '../../lib/download-blob';
+import { projectTitleInitialState } from '../../reducers/project-title';
+import { getIsShowingProject } from '../../reducers/project-state';
 
 import styles from './crash-message.css';
 import reloadIcon from './reload.svg';
-import SB3Downloader from '../../containers/sb3-downloader.jsx'
 
+const getProjectFilename = (curTitle, defaultTitle) => {
+    let filenameTitle = curTitle;
+    if (!filenameTitle || filenameTitle.length === 0) {
+        filenameTitle = defaultTitle;
+    }
+    return `${filenameTitle.substring(0, 100)}.sb3`;
+};
 
-const CrashMessage = props => (
+const CrashMessage = ({eventId, errorMessage, onReload, saveProjectSb3, projectFilename}) => (
     <div className={styles.crashWrapper}>
         <Box className={styles.body}>
             <img
@@ -26,32 +36,32 @@ const CrashMessage = props => (
             <p>
                 <FormattedMessage
                     defaultMessage={'We are so sorry, but it looks like the page has crashed.' +
-                        ' Please refresh your page to try' +
-                        ' again.'}
+                    ' Please refresh your page to try' +
+                    ' again.'}
                     description="Message to inform the user that page has crashed."
                     id="tw.gui.crashMessage.description"
                 />
             </p>
-            {props.errorMessage && (
+            {errorMessage && (
                 <p className={styles.errorMessage}>
-                    {props.errorMessage}
+                    {errorMessage}
                 </p>
             )}
-            {props.eventId && (
+            {eventId && (
                 <p>
                     <FormattedMessage
                         defaultMessage="Your error was logged with id {errorId}"
                         description="Message to inform the user that page has crashed."
                         id="gui.crashMessage.errorNumber"
                         values={{
-                            errorId: props.eventId
+                            errorId: eventId
                         }}
                     />
                 </p>
             )}
             <button
                 className={styles.reloadButton}
-                onClick={props.onReload}
+                onClick={onReload}
             >
                 <FormattedMessage
                     defaultMessage="Reload"
@@ -59,20 +69,22 @@ const CrashMessage = props => (
                     id="gui.crashMessage.reload"
                 />
             </button>
-            <SB3Downloader>
-                {(_className, _downloadProject, {smartSave}) => (
-                    <button
-                        className={styles.reloadButton}
-                        onClick={smartSave}
-                    >
-                        <FormattedMessage
-                            defaultMessage="Try to Save Project"
-                            description="Button to try to save project"
-                            id="gui.crashMessage.saveproject"
-                        />
-                    </button>
-                )}
-            </SB3Downloader>
+            {saveProjectSb3 && (
+                <button
+                    className={styles.reloadButton}
+                    onClick={() => {
+                        saveProjectSb3().then(content => {
+                            downloadBlob(projectFilename, content);
+                        });
+                    }}
+                >
+                    <FormattedMessage
+                        defaultMessage="Try to Save Project"
+                        description="Button to try to save project"
+                        id="gui.crashMessage.saveproject"
+                    />
+                </button>
+            )}
         </Box>
     </div >
 );
@@ -80,7 +92,14 @@ const CrashMessage = props => (
 CrashMessage.propTypes = {
     eventId: PropTypes.string,
     errorMessage: PropTypes.string,
-    onReload: PropTypes.func.isRequired
+    onReload: PropTypes.func.isRequired,
+    saveProjectSb3: PropTypes.func,
+    projectFilename: PropTypes.string
 };
 
-export default CrashMessage;
+const mapStateToProps = state => ({
+    saveProjectSb3: state.scratchGui.vm.saveProjectSb3.bind(state.scratchGui.vm),
+    projectFilename: getProjectFilename(state.scratchGui.projectTitle, projectTitleInitialState)
+});
+
+export default connect(mapStateToProps)(CrashMessage);
