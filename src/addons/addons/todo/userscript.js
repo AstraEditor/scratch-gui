f93d7f712611c1a7dd29170ffa298e75c250d565
KOSHINO <2381068747@qq.com>
2026-04-19 19:39:22 +0800

    Todo addon: Add new todo!

--- src/addons/addons/todo/userscript.js
+++ src/addons/addons/todo/userscript.js
@@ -5,24 +5,20 @@ import SideBar from "../../ui/side-bar/side-bar.js";
 /*
 {
   groups: [
-    { id: "g1", name: "工作", color: "#3b82f6", order: 0 }
+    { id: "g1", name: "工作", color: "#3b82f6"}
   ],
   tasks: [
     {
       id: "t1",
       name: "写周报",
-      startTime: "2026-04-19T09:00",
-      endTime: "2026-04-19T18:00",
+      startTime: "123445",
+      endTime: "33333",
       done: false,
-      groupId: "g1",
       priority: 2,
-      tags: ["办公"],
+      tags: ["g1"],
       steps: [
         { id: "s1", text: "收集数据", done: true }
       ],
-      order: 0,
-      remindAt: "2026-04-19T08:00",
-      repeat: null
     }
   ]
 }
@@ -31,6 +27,7 @@ import SideBar from "../../ui/side-bar/side-bar.js";
 const SIDEBAR_ID = 'todo';
 var ADDON = null;
 var PROJECT_NAME = '';
+var MSG; // msg函数
 const POINT = '_TODO_LIST_'
 const emptyTodo = {
     groups: [],
@@ -52,34 +49,51 @@ const createSideBarElements = (msg) => {
     const createButton = document.createElement('button');
     createButton.textContent = msg('createTodo');
     createButton.onclick = () => {
-        createCommentToStage(getFormatComment(JSON.stringify(emptyTodo)))
+        createCommentToStage(getFormatComment(emptyTodo))
     }
 
-    const todoList = document.createElement('div');
+    const todoList = document.createElement('ul');
     try {
-        console.log(getTodoListContent())
-        getTodoListContent().forEach(element => {
-            const todoEle = document.createElement('div');
-            todoEle.textContent = element;
+        getTodoListContent().tasks.forEach(element => {
+            const todoEle = document.createElement('li');
+            todoEle.textContent = element.name;
 
             todoList.appendChild(todoEle);
         });
     } catch (e) {
         console.warn('Todo List can\'t display')
     }
+    const testButton = document.createElement('button');
+    testButton.textContent = 'test';
+    testButton.onclick = () => {
+        addNewTodo({
+            mode: 2,
+            name: 'abc',
+            task:{
+                startTime: Date.now(),
+                endTime: Date.now() + 100000086,
+                done: false,
+                tags: [],
+            }
+        })
+    }
 
     content.appendChild(title)
     content.appendChild(createButton)
+    content.appendChild(testButton)
+    content.appendChild(todoList)
     return content
 }
 const getFormatComment = content => `
 This comment is for the "todo" addon, this comment will storage your to-do list.\n
 So don't edit, remove it. But you can move, resize and hide it, it won't affect work.
 ${POINT}
-${content}
+${JSON.stringify(content)}
 `
 const createCommentToStage = content => {
     const vm = ADDON.tab.traps.vm;
+    // 删除之前的comment,它实际上不会替换
+    delete vm.runtime.getTargetForStage().comments['todo']
     vm.runtime.getTargetForStage().createComment(
         'todo',
         null,
@@ -92,6 +106,52 @@ const createCommentToStage = content => {
     );
 }
 
+/**
+ * 添加新的Todo
+ * @param {object} config 配置
+ * @param {1|2} config.mode - 1为加入组（group），2为加入todo（tasks）
+ * @param {string} config.id - ID，用于区分
+ * @param {string} config.name - 对组的配置
+ * @param {object} config.task - 对todo的配置
+ * @param {int} config.task.startTime - 开始时间
+ * @param {int} config.task.endTime - 结束时间
+ * @param {boolean} config.task.done - 是否完成
+ * @param {[]} config.task.tags - 属于什么组
+ * @param {int} config.task.priority - 优先级，越高越提前，默认为0
+ * @param {[{ id: string, text: string, done: boolean }]} config.task.steps - 步骤
+ * @param {object} config.group - 对组的配置
+ * @param {string} config.group.color - 显示的颜色
+ */
+const addNewTodo = config => {
+    const editTodo = getTodoListContent();
+    if (config.mode === 1) {
+        // 对于group
+        editTodo.groups = [
+            ...editTodo.groups,
+            {
+                id: config.id || Math.random(),
+                name: config.name || MSG("New Group"),
+                color: config.group.color || '#0099ff'
+            }
+        ]
+    } else if (config.mode === 2) {
+        // 对于Task
+        editTodo.tasks = [
+            ...editTodo.tasks,
+            {
+                id: config.id || Math.random(),
+                name: config.name || MSG("New Group"),
+                startTime: config.task.startTime || Date.now(),
+                endTime: config.task.endTime || Date.now() + 100000086,
+                done: config.task.done || false,
+                groupId: config.task.tags || [],
+                steps: config.task.steps || []
+            }
+        ]
+    }
+    createCommentToStage(getFormatComment(editTodo))
+}
+
 const getTodoList = () => {
     const vm = ADDON.tab.traps.vm;
     return vm.runtime.getTargetForStage().comments['todo'] || ''
@@ -100,7 +160,7 @@ const getTodoListContent = () => {
     try {
         return JSON.parse(
             getTodoList()['text']
-            .split(POINT)[1] 
+                .split(POINT)[1]
             /**
              * 我们的格式是:
              * 
@@ -112,7 +172,7 @@ const getTodoListContent = () => {
              */
         )
     } catch (e) {
-        return []
+        return emptyTodo
     }
 }
 
@@ -171,6 +231,7 @@ function addButton(msg) {
 
 }
 export default function ({ addon, msg }) {
+    MSG = msg
     ADDON = addon;
     if (isVSCodeLayout()) addButtonWithVSCodeLayout(msg)
     else addButton(msg)
