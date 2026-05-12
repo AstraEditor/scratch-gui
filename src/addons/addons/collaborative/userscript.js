c77c514c3b6345723303806c91f87e3222825461
KOSHINO <2381068747@qq.com>
2026-05-12 22:52:28 +0800

    feat(coll addon): link to localServer

--- src/addons/addons/collaborative/userscript.js
+++ src/addons/addons/collaborative/userscript.js
@@ -1,6 +1,7 @@
 import addToBar from '../../tools/AddToBar';
 import { getSetting } from '../../tools/AEsettings';
 import icon from '!../../../lib/tw-recolor/build!./icon.svg';
+import { text } from '../../../../../scratch-vm/src/extension-support/tw-default-extension-urls';
 /*
 联机编辑
 {
@@ -19,6 +20,9 @@ export default async function ({ addon, console, msg }) {
     const vm = addon.tab.traps.vm;
     if (!vm) return;
     const tabID = 'sa-addon-collaborative-tab';
+    const COMMAND = {
+        ENTER: 'join'
+    }
 
     let port = 1832;
     // 连接的服务器
@@ -44,6 +48,29 @@ export default async function ({ addon, console, msg }) {
             box.appendChild(content);
             return box;
         }
+        /**
+         * @param {Object} config 
+         * @param {String} config.defaultValue
+         * @param {'text' | 'number'} config.type
+         * @param {String} config.label
+         * @param {Function} config.onChange
+         * @param {String} text 
+         */
+        const inputBox = (config, text) => {
+            const box = document.createElement('div');
+            box.className = 'sa-addon-collaborative-inputBox';
+            const label = document.createElement('label');
+            label.textContent = config.label;
+            const input = document.createElement('input');
+            input.type = config.type;
+            input.value = text;
+            input.onchange = e => {
+                config.onChange(e.target.value);
+            }
+            box.appendChild(label);
+            box.appendChild(input);
+            return box;
+        }
 
         const Container = document.createElement('div');
         Container.className = 'sa-addon-collaborative-container';
@@ -53,28 +80,41 @@ export default async function ({ addon, console, msg }) {
         Title.textContent = msg('title');
         Title.className = 'sa-addon-collaborative-title';
 
-        // 端口输入
-        const portInput = document.createElement('input');
-        portInput.type = 'number';
-        portInput.value = port;
-        portInput.className = 'sa-addon-collaborative-portInput';
-        portInput.onChange = e => {
-            port = e.target.value;
+        const joinButton = document.createElement('button');
+        joinButton.textContent = msg('join');
+        joinButton.onclick = () => {
+            login()
         }
 
         if(isVSCL) Container.appendChild(Title);
         Container.appendChild(tipBox('tip', msg('alpha_warn')))
-        Container.appendChild(portInput);
+        Container.appendChild(inputBox({
+                type: 'number',
+                label: msg('port'),
+                value: port,
+                onChange: value => {
+                    port = value;
+                }
+            }, port.toString()));
+        Container.appendChild(joinButton);
         return Container;
     }
 
     const login = async () => {
         server = new WebSocket(`ws://localhost:${port}`);
         server.onopen = () => {
-            server.send("Hello World");
+            server.send(JSON.stringify({ //进入房间
+                com: COMMAND.ENTER,
+                name: localStorage.getItem('tw:username') || "user",
+                id: Date.now()
+            }));
         };
+        server.onmessage = msgs => {
+            const data = JSON.parse(msgs.data)
+            console.log(data)
+            if(data.info) alert(data.info)
+        }
     }
-    login()
 
     addToBar(addon, {
         id: tabID,
