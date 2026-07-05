0dc23e215d222a9deb984cae5ea29113ae927ab4
KOSHINO <2381068747@qq.com>
2026-07-05 16:53:54 +0800

    v1.2.6

--- src/lib/ae-version.js
+++ src/lib/ae-version.js
@@ -1,11 +1,12 @@
 const version = {
-    version: '1.2.5',
-    date: '2026-06-28',
+    version: '1.2.6',
+    date: '2026-07-05',
     webBuild: '9',
     webUpdate: [
-        '修复窗口控制按钮的诸多问题',
-        '修复TabList中插件的显示顺序问题',
-        '修复舞台区的宽度问题'
+        "修复打包器无法使用的问题",
+        "修复窗口控制按钮的颜色问题(AstraEditor/scratch-gui#22)",
+        "修复布局的一堆问题(AstraEditor/scratch-gui#18)",
+        "修复“hide menubar”插件的一些问题(AstraEditor/scratch-gui#18)"
     ]
 };
 export default version;
